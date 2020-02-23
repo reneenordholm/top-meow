e0940034dcc6b77b2a46f73b1905c0253be104a4
Renee Nordholm <reneenordholm@gmail.com>
2020-02-22 21:09:39 -0600

    controller/routes  setup

--- backend/app/controllers/cats_controller.rb
+++ backend/app/controllers/cats_controller.rb
@@ -3,4 +3,40 @@ class CatsController < ApplicationController
         cats = Cat.all
         render json: cats.to_json(:except => [:created_at, :updated_at])
     end
+
+    def show
+        cat = Cat.find_by(img_id: params[:id])
+        if cat.nil?
+            render json: {messages: "cat does not exist"}
+        else
+            render json: cat.to_json(:except => [:created_at, :updated_at]) 
+        end
+    end
+
+    def create
+        cat = Cat.find_by(img_id: params[:img_id])
+
+        if cat
+            render :update
+        else
+            cat = Cat.create(cat_params.merge(id: cat.id))
+        end
+    end
+
+    def update
+        cat = Cat.find_by(img_id: params[:img_id])
+        cat.update(cat_params)
+
+        if cat.save
+            render json: {messages: "cat saved"}
+        else
+            render json: {errors: "cat not saved"}
+        end
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:img_id, :url, :likes)
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :cats, only: [:index]
+  resources :cats, only: [:index, :show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
