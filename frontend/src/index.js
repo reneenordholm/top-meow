0b08dbe20f5d45f8e7f889b085517d5373dc78c6
Renee Nordholm <reneenordholm@gmail.com>
2020-02-24 14:02:30 -0600

    index.js/store set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import cats from './reducers/cats'
+import cats from './reducers/catsReducer'
 
-let store = createStore(cats)
+import App from './App';
+
+//setup browser devtools extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// setup store--pass in reducer and thunk middleware
+let store = createStore(catsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
-  // </Provider>,
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
