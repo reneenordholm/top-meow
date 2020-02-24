4ebe7b8246f35cc9ad698ceaec7a2f98f022ac08
Renee Nordholm <reneenordholm@gmail.com>
2020-02-24 13:39:41 -0600

    import applyMiddleware to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import cats from './reducers/cats'
+import cats from './reducers/cats'
 
-// let store = createStore(cats)
+let store = createStore(cats)
 
 ReactDOM.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <App />,
   // </Provider>,
   document.getElementById('root')
