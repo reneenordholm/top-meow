a9b77f7a2a0a30c8b4570d4e46a9dcb47a1fa145
Renee Nordholm <reneenordholm@gmail.com>
2020-02-24 14:08:30 -0600

    frontend directory setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,9 @@ import App from './App';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // setup store--pass in reducer and thunk middleware
+// store globally stores data
+// reducer takes in action object and updates store accordingly
+// thunk allows for asychronous fetches
 let store = createStore(catsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
