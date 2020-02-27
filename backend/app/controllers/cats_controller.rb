d48d65061d84d9221cd1ad8ba2bbf359d4315343
Renee Nordholm <reneenordholm@gmail.com>
2020-02-27 13:25:50 -0600

    like/dislikes successfully persists to db

--- backend/app/controllers/cats_controller.rb
+++ backend/app/controllers/cats_controller.rb
@@ -4,33 +4,14 @@ class CatsController < ApplicationController
         render json: cats.to_json(:except => [:created_at, :updated_at])
     end
 
-    def show
-        cat = Cat.find_by(img_id: params[:id])
-        if cat.nil?
-            render json: {messages: "cat does not exist"}
-        else
-            render json: cat.to_json(:except => [:created_at, :updated_at]) 
-        end
-    end
-
     def create
         cat = Cat.find_by(img_id: params[:img_id])
 
-        if cat
-            render :update
+        if cat != nil
+            cat.likes = cat.likes + params[:likes]
+            cat.update(cat.likes)
         else
-            cat = Cat.create(cat_params.merge(id: cat.id))
-        end
-    end
-
-    def update
-        cat = Cat.find_by(img_id: params[:img_id])
-        cat.update(cat_params)
-
-        if cat.save
-            render json: {messages: "cat saved"}
-        else
-            render json: {errors: "cat not saved"}
+            Cat.create(cat_params)
         end
     end
 
--- frontend/src/actions/updateLike.js
+++ frontend/src/actions/updateLike.js
@@ -1,8 +1,18 @@
 // receive cat data from once state is updated in catlikes
 // using thunk to dispatch 
 export const updateLike = cat => {
-    return (dispatch) => 
-        console.log(cat)    
+    return (dispatch) => {
+        fetch('http://localhost:3001/cats', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(cat)
+        })
+        // .then (res => res.json())
+    }
+           
     // {
     //     type: 'UPDATE_LIKE',
     //     cat
