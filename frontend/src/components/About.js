8e58f3a9179e75098880a39b0a6ec0cd53c6f249
Renee Nordholm <reneenordholm@gmail.com>
2020-03-10 23:17:15 -0500

    about page description update

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,6 +6,7 @@ const About = () => {
     <div className="w3-content">
       <div className="w3-display-container w3-content w3-wide" id="home">
         <h1 className="w3-center">About Top Meow</h1>
+        <p className="w3-center">This is the top meow about page.</p>
       </div>
     </div>
   )
