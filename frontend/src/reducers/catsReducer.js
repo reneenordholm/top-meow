412c9b6a9aad7a8bcc75e03383a2f5ecbb768ca1
Renee Nordholm <reneenordholm@gmail.com>
2020-03-12 13:38:09 -0500

    adding notes

--- frontend/src/reducers/catsReducer.js
+++ frontend/src/reducers/catsReducer.js
@@ -5,16 +5,20 @@
 export default function catsReducer(state = {cats: []}, action) {
     switch(action.type) {
         case 'FETCH_CATS':
-            // fetch cat from cat api
+            // fetch cat from api
             return {cats: action.payload}
         case 'UPDATE_LIKE':
+            // iterate through each cat
             let cats = state.cats.map(cat => {
+                // when cat img_id matches the img_id in payload
                 if (cat.img_id === action.payload.img_id) {
+                  // update that cats information
                   return action.payload
                 } else {
                   return cat
                 }
               }) 
+              // return new copy of state since state is immutable
               return {...state, cats: cats}
         default:
             // make sure something is always returned
